c21842838266eb48eca69e08342443aecfa8283d
CristelRR <cristel23rr@gmail.com>
2024-02-29 12:41:49 -0600

    first commit

---
+++ dist/hola.js
@@ -0,0 +1,4 @@
+//Lucía Cristel Ramírez Romero
+let mensaje = "Holis Bolis!!!";
+console.log(mensaje);
+//# sourceMappingURL=hola.js.map
---
+++ src/hola.ts
@@ -0,0 +1,4 @@
+//Lucía Cristel Ramírez Romero
+let mensaje: string = "Holis Bolis!!!"
+
+console.log(mensaje)
